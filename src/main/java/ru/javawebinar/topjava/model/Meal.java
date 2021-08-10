b248047ab435c519914e75ec876faeda2f5b74ed
id2k1149 <id2k1149@gmail.com>
2021-08-09 21:09:09 -0400

    message for calories in Meal

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -44,7 +44,7 @@ public class Meal extends AbstractBaseEntity {
     @Column(name = "calories", nullable = false)
     @NotBlank
     @Range(min = 10, max = 5000, message = "calories must be between 10 and 5000")
-    private int calories;
+    private Integer calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
@@ -71,7 +71,7 @@ public class Meal extends AbstractBaseEntity {
         return description;
     }
 
-    public int getCalories() {
+    public Integer getCalories() {
         return calories;
     }
 
@@ -91,7 +91,7 @@ public class Meal extends AbstractBaseEntity {
         this.description = description;
     }
 
-    public void setCalories(int calories) {
+    public void setCalories(Integer calories) {
         this.calories = calories;
     }
 
